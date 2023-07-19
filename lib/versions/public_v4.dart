c6de301ccbf30002d321f64cb7c8ca5420953451
Phil Larson <phil.larson@icloud.com>
2023-07-19 06:48:49 -0700

    PublicV4

--- lib/versions/public_v4.dart
+++ lib/versions/public_v4.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:cryptography/cryptography.dart';
 import 'package:meta/meta.dart';
 import 'package:paseto/paseto.dart';
@@ -14,46 +17,47 @@ class PublicV4 {
     Token token, {
     required PublicKey publicKey,
   }) async {
-    throw UnimplementedError();
+    final payload = token.payloadPublic;
+    if (payload == null) {
+      throw UnsupportedError('Invalid payload');
+    }
+    final isValid = await Ed25519().verify(
+      token.standardPreAuthenticationEncoding,
+      signature: Signature(
+        payload.signature!,
+        publicKey: publicKey,
+      ),
+    );
+    if (!isValid) {
+      throw Exception('Invalid signature');
+    }
+    return Package(
+      content: payload.message,
+      footer: token.footer,
+    );
+  }
 
-    // final payload = token.payloadPublic;
-    // if (payload == null) {
-    //   throw UnsupportedError('Invalid payload');
-    // }
-    // final isValid = await Ed25519().verify(
-    //   token.standardPreAuthenticationEncoding,
-    //   signature: Signature(
-    //     payload.signature!,
-    //     publicKey: publicKey,
-    //   ),
-    // );
-    // if (!isValid) {
-    //   throw Exception('Invalid signature');
-    // }
-    // return Package(
-    //   content: payload.message,
-    //   footer: token.footer,
-    // );
+  Uint8List extractBytesFromPrivateKey(String privateKeyString) {
+    return Uint8List.fromList(utf8.encode(privateKeyString));
   }
 
   static Future<Payload> sign(
     Package package, {
     required KeyPair keyPair,
   }) async {
-    throw UnimplementedError();
-
-    // final signature = await Ed25519().sign(
-    //   Token.preAuthenticationEncoding(
-    //     header: PublicV4.header,
-    //     payload: PayloadPublic(message: package.content),
-    //     footer: package.footer,
-    //     implicit: [],
-    //   ),
-    //   keyPair: keyPair,
-    // );
-    // return PayloadPublic(
-    //   message: package.content,
-    //   signature: signature.bytes,
-    // );
+    final preAuthenticationEncoding = Token.preAuthenticationEncoding(
+      header: PublicV4.header,
+      payload: PayloadPublic(message: package.content),
+      footer: package.footer,
+      implicit: [],
+    );
+    final signature = await Ed25519().sign(
+      preAuthenticationEncoding,
+      keyPair: keyPair,
+    );
+    return PayloadPublic(
+      message: package.content,
+      signature: signature.bytes,
+    );
   }
 }
