9477b85030dff40949549641d3f4615fe936dc5c
Phil Larson <phil.larson@icloud.com>
2023-07-19 03:20:12 -0700

    UnimplementedError

--- lib/versions/local_v4.dart
+++ lib/versions/local_v4.dart
@@ -17,81 +17,14 @@ class LocalV4 {
     Token token, {
     required SecretKey secretKey,
   }) async {
-    final payload = token.payloadLocal;
-    if (payload == null) throw UnsupportedError('Invalid payload type.');
-    final secretBox = payload.secretBox;
-    final nonce = payload.nonce;
-    if (nonce == null) {
-      throw Exception('Missing nonce');
-    }
-    if (secretBox == null) {
-      throw Exception('Missing secretBox');
-    }
-    final cipher = AesCtr.with256bits(
-      macAlgorithm: Hmac.sha384(),
-    );
-    final subkeys = await secretKey.deriveSubkeysV4(nonce: nonce);
-    final mac = await cipher.macAlgorithm.calculateMac(
-      Token.preAuthenticationEncoding(
-        header: header,
-        payload: payload,
-        footer: token.footer,
-      ),
-      secretKey: subkeys.authenticationKey,
-    );
-    if (Hash(mac.bytes) != payload.mac) {
-      throw Exception('Invalid mac');
-    }
-    final content = await cipher.decrypt(
-      SecretBox(
-        secretBox.cipherText,
-        nonce: subkeys.nonce2.bytes,
-        mac: Mac.empty,
-      ),
-      secretKey: subkeys.encryptionKey,
-    );
-    return Package(
-      content: content.sublist(0, content.length - mac.bytes.length),
-      footer: token.footer,
-    );
+    throw UnimplementedError();
   }
 
   static Future<Payload> encrypt(
     Package package, {
     required SecretKey secretKey,
   }) async {
-    final cipher = AesCtr.with256bits(
-      macAlgorithm: Hmac.sha384(),
-    );
-    final nonceKey = await cipher.newSecretKey();
-    final nonce = Mac(await nonceKey.extractBytes());
-    final subkeys = await secretKey.deriveSubkeysV4(nonce: nonce);
-    final secretBox = await cipher.encrypt(
-      package.content,
-      nonce: subkeys.nonce2.bytes,
-      secretKey: subkeys.encryptionKey,
-    );
-    final prePayload = PayloadLocal(
-      nonce: nonce,
-      secretBox: SecretBox(
-        secretBox.cipherText + secretBox.mac.bytes,
-        nonce: secretBox.nonce,
-        mac: secretBox.mac,
-      ),
-    );
-    final mac = await cipher.macAlgorithm.calculateMac(
-      Token.preAuthenticationEncoding(
-        header: header,
-        payload: prePayload,
-        footer: package.footer,
-      ),
-      secretKey: subkeys.authenticationKey,
-    );
-    return PayloadLocal(
-      nonce: prePayload.nonce,
-      secretBox: prePayload.secretBox,
-      mac: Hash(mac.bytes),
-    );
+    throw UnimplementedError();
   }
 }
 
